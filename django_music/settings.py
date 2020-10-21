6bf68d066c1451fb57612d1f840ee0fd244fffbc
Siquoyia <70215872+Siquoyia@users.noreply.github.com>
2020-10-21 14:23:58 -0700

    django music

--- django_music/settings.py
+++ django_music/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'registration',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,6 +52,9 @@ INSTALLED_APPS = [
 
     # Project-specific
     'users',
+    'albums',
+    
+
 ]
 
 MIDDLEWARE = [
